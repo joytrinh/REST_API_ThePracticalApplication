42c96b93bc0dd3ea082b7893bd6284f5d275858e
joytrinh <mongtuyen149@gmail.com>
2020-06-10 22:03:54 +0700

    Handle errors

--- backend-starting-setup/app.js
+++ backend-starting-setup/app.js
@@ -47,7 +47,16 @@ app.use((req, res, next) => {
 app.use('/graphql', graphqlHttp({
   schema: graphqlSchema,
   rootValue: graphqlResolver,
-  graphiql: true
+  graphiql: true,
+  customFormatErrorFn(err) { //"`formatError` is deprecated and replaced by `customFormatErrorFn`. It will be removed in version 1.0.0."
+    if (!err.originalError) {
+      return err
+    }
+    const data = err.originalError.data
+    const message = err.message || 'An error occurred.'
+    const code = err.originalError.code || 500
+    return { message: message, status: code, data: data}
+  }
 }))
 
 app.use((error, req, res, next) => {
